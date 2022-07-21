7c9e0db4cbb916c5ea991719e5275ed19580a2f5
David <brushdm@gmail.com>
2022-07-20 21:54:01 -0400

    First commit for Lab 8.1. Totally lost. Like J.J. Abrams had a baby with Will Robinson, then they lost it lost.

---
+++ WeatherAPI/src/main/java/co/grandcircus/WeatherAPI/WeatherController.java
@@ -0,0 +1,14 @@
+package co.grandcircus.WeatherAPI;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WeatherController {
+	
+	@RequestMapping("/")
+	public String homePage() {
+		return "home";
+	}
+
+}
---
+++ WeatherAPI/src/main/java/co/grandcircus/WeatherAPI/WeatherResponse.java
@@ -0,0 +1,9 @@
+package co.grandcircus.WeatherAPI;
+
+public class WeatherResponse {
+	
+	private String type;
+	
+	
+
+}
